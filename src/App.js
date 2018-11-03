eeb4bf65dc1b856eafc346d5fd859047399da02f
github username <github email address>
2018-11-03 14:43:25 -0500

    setup react app

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import SpotifyAuth from "./components/SpotifyAuth";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <SpotifyAuth />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/SpotifyAuth.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function SpotifyAuth() {
+  return (
+    <div>
+      <p>Spotify Auth</p>
+    </div>
+  );
+}
+
+export default SpotifyAuth;
