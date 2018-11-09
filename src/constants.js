887a7738f66f13797df4e00ceb326ee32aefdcb1
github username <github email address>
2018-11-09 15:13:21 -0600

    TV_API constant not working switched to api.tunevoter.com

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,4 @@
-const TV_API = process.env.NODE_ENV !== 'production' ? "http://localhost:3000" : "http://api.tunevoter.com" 
+// const TV_API = process.env.NODE_ENV !== 'production' ? "http://localhost:3000" : "http://api.tunevoter.com"
+const TV_API = "http://api.tunevoter.com";
 
-export {TV_API}
+export { TV_API };
